cd862e187dd649e9769f6dbfcfec187eeef80c5a
Alvaro Nieto <60838509+Anieto86@users.noreply.github.com>
2020-06-21 17:40:23 +0930

    query, is okay?

--- model/workout.js
+++ model/workout.js
@@ -2,40 +2,53 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const Workout = new Schema({
-    type: {
-        type: String,
-
+const workoutSchema = new Schema({
+    
+    day:{
+        type: Number,
     },
-    name:{
-        type: String, 
-
-    },
-    duration: {
-        type: Number, 
-        required: "Time"
-    },
-    weight: {
-        type: Number, 
-        
-
-    },
-    reps: {
-        type: Number, 
-        required: "Number of Reps"
-
-    },
-    sets:{
-        type: Number, 
-        required: "Number of Sets"
-    },
-
-
-
+    
+    
+    Exercises: [{
+        type: {
+            type: String,
+        },
+        name: {
+            type: String,
+        },
+        duration: {
+            type: Number,
+            required: "Time",
+        },
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+            required: "Number of Reps"
+        },
+        sets: {
+            type: Number,
+            required: "Number of Sets"
+        }
+    }]
 });
 
 
-const Workout =  mongoose.model("Exercises" , Workout);
+// or  
+
+// exercises: [
+//     {
+//       type: String,
+//       name: String,
+//       duration: Number,
+//       weight:  Number,
+//       reps:  Number,
+//       sets:  Number
+//     }
+//   ]
+
+const workoutSchema = mongoose.model("workout", workoutSchema);
 
 
-module.exports = Workout;
+module.exports = workout;
